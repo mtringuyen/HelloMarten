916118105be23b8b8e181f878a69f5712d4d2fbd
Minh Tri Nguyen <minhtri.mtn@hotmail.com>
2017-07-09 22:58:03 +0700

    add model controler

---
+++ HelloMarten/Controllers/BlogPostController.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using HelloMarten.Model;
+
+namespace HelloMarten.Controllers
+{
+  [Route("/posts")]
+  public class BlogPostController
+  {
+    private readonly IDocumentStore _documentStore;
+
+    public BlogPostController(IDocumentStore documentStore)
+    {
+      _documentStore = documentStore;
+    }
+
+    [HttpGet]
+    public IEnumerable<BlogPost> Get()
+    {
+      using (var session = _documentStore.QuerySession())
+      {
+        return session.Query<BlogPost>();
+      }
+    }
+
+    [HttpGet("{id}")]
+    public BlogPost Get(int id)
+    {
+      using (var session = _documentStore.QuerySession())
+      {
+        return session
+            .Query<BlogPost>()
+            .Where(post => post.Id == id)
+            .FirstOrDefault();
+
+      }
+    }
+
+    [HttpPost]
+    public BlogPost Create([FromBody]BlogPost post)
+    {
+      using (var session = _documentStore.LightweightSession())
+      {
+        session.Store(post);
+        session.SaveChanges();
+        return post;
+      }
+    }
+  }
+}
